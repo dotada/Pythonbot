4e83cbed31930878a33807e9494cdead87c518ba
test mafoam <65085906+dotada@users.noreply.github.com>
2021-09-09 16:16:18 +0000

    reached over 100 lines bois

--- main.py
+++ main.py
@@ -3,7 +3,6 @@ import keep_alive
 import random
 from discord.ext import commands
 
-
 bot = commands.Bot(command_prefix=';')
 TOKEN = os.environ['TOKEN']
 
@@ -68,11 +67,39 @@ async def inspect(ctx):
   await ctx.send("https://cdn.discordapp.com/attachments/885182840738816001/885308611079847976/Screenshot_1625.png")
 
 @bot.command()
-async def r34(ctx):
+async def r(ctx):
   b = random.randint(0000000, 4480000)
   number = str(b)
   url = "https://rule34.xxx/index.php?page=post&s=view&id=" + number
   await ctx.send(url)
 
+@bot.command()
+async def e(ctx):
+  v = random.randint(0000000, 2500000)
+  y = str(v)
+  url2 = "https://e621.net/posts/" + y
+  await ctx.send(url2)
+
+@bot.command()
+async def b(ctx):
+  n = random.randint(0000000, 6100000)
+  k = str(n)
+  url3 = "https://e926.net/posts/" + k
+  await ctx.send(url3)
+
+@bot.command()
+async def za(ctx):
+  await ctx.send("warudo")
+
+@bot.command()
+async def star(ctx):
+  await ctx.send("platinum")
+
+@bot.command()
+async def nh(ctx):
+  g = random.randint(0o00002, 366000)
+  o = str(g)
+  url4 = "https://nhentai.to/g/" + o
+  await ctx.send(url4)
 keep_alive.keep_alive()
 bot.run(TOKEN)
